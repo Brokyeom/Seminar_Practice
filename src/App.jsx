63cca19c40e3cf25c66fce1a063e4c4b0dc14873
Brokyeom <goindol40@gmail.com>
2024-04-04 00:09:30 +0900

    chore : install styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from "styled-components";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <StMain>
+      <StButton>Hello Web!</StButton>
+    </StMain>
+  );
 }
 
-export default App
+export default App;
+
+const StMain = styled.main`
+  display: flex;
+  justify-content: center;
+  align-item: center;
+`;
+
+const StButton = styled.button`
+  color: white;
+  background-color: tomato;
+
+  font-size: 2rem;
+
+  padding: 1rem 0.5rem 1rem 0.5rem;
+
+  border: none;
+`;
