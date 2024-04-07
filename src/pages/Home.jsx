015f69816c1b119b20932e9d46f9066fad9fdfcd
Brokyeom <goindol40@gmail.com>
2024-04-07 16:23:23 +0900

    chroe : set eslint and perttier

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Home = () => {
+  return (
+
+        <StMain>
+          <StButton>Hello Web!</StButton>
+        </StMain>
+  )
+}
+
+const StMain = styled.main`
+  display: flex;
+  justify-content: center;
+  align-item: center;
+`;
+
+const StButton = styled.button`
+  color: white;
+  background-color: tomato;
+
+  font-size: 2rem;
+  
+  padding: 1rem 0.5rem 1rem 0.5rem;
+
+  border: none;
+`;
